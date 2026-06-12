3166131fe834bd05095ff96da937d1f3951e5f94
tiagomdr <tiago335tm@gmail.com>
2026-06-12 17:30:33 -0300

    Enviador Projeto do Luigi

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const formulario = document.querySelector('.formulario')
+const mascara = document.querySelector('.mascara')
+
+
+function mostrarForm() {
+    formulario.style.left = '50%'
+    formulario.style.transform = 'translateX(-50%)'
+    mascara.style.zIndex = '1'
+    mascara.style.display = 'block'
+    formulario.style.transition = '0.8s ease-in-out'
+}
+
+
+function desaparecerForm() {
+     formulario.style.left = '-342px'
+     mascara.style.display = 'none'
+     formulario.style.transition = '0.8s ease-in'
+}
