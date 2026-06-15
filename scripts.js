0e54dd5539591827e3f6cccee3ff330e84204b4e
tiagomdr <tiago335tm@gmail.com>
2026-06-15 10:04:14 -0300

    Atulizando Projeto

--- scripts.js
+++ scripts.js
@@ -5,14 +5,15 @@ const mascara = document.querySelector('.mascara')
 function mostrarForm() {
     formulario.style.left = '50%'
     formulario.style.transform = 'translateX(-50%)'
-    mascara.style.zIndex = '1'
-    mascara.style.display = 'block'
-    formulario.style.transition = '1s ease-in'
+    mascara.style.visibility = 'visible'
 }
 
 
 function desaparecerForm() {
      formulario.style.left = '-342px'
-     mascara.style.display = 'none'
-     formulario.style.transition = '1s ease'
+     mascara.style.visibility = 'hidden'
+}
+
+function linkpararepositorio() {
+    window.open('https://github.com/tiagomdr/Projeto-do-Luigi', '_blank')
 }
